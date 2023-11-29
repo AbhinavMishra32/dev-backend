e780db391024b80d72984af1e2645aa274136725
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-11-30 00:18:41 +0530

    Add API endpoint to store data in database

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ app.listen(3000, ()=> {
 app.use(express.json({limit: '1mb'}));
 app.use(express.static('public'));
 
+const databaseArray = [];
+
 app.post('/api', (request, response) => {
     console.log('I got a request!')
-    console.log()
     console.log(request.body);
-})
 
+    databaseArray.push(request.body);
+
+    response.json({
+        status: 'success',
+        latitude: request.body.lat, 
+        longitude: request.body.lon,
+        database: databaseArray,
+        length: databaseArray.length
+    })
+
+    console.log(databaseArray)
+})
