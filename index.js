227e9d798f423207b5284472a65d1f8f6ef2cbd2
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2023-11-29 22:25:48 +0530

    Add API endpoint for receiving location data

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ const app = express();
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
 });
-app.use(express.static('server-application/public'));
+app.use(express.json({limit: '1mb'}));
+app.use(express.static('public'));
+
+app.post('/api', (request, response) => {
+    console.log('I got a request!')
+    console.log()
+    console.log(request.body);
+})
+
